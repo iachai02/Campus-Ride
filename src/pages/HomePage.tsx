915f52f0f789af6797dda1c66b933e6f3f34ff22
Immanuel Chai <immanuelchai7@gmail.com>
2024-09-16 14:51:55 -0700

    adjusted global, homepage.tsx and .css and signup.css
    adjusted the css for homepage so that it doesnt interfere with the other pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,18 +4,16 @@ import "../css/HomePage.css";
 function HomePage() {
   return (
     <div className="home-page">
-      <header className="home-header">
-        <h1>Welcome to UCSD Campus Ride</h1>
-        <p>
-          <span className="special-text">Organize rides to campus events.</span>{" "}
-          Sign up as a <strong>driver</strong> or find a ride as a{" "}
-          <strong>passenger</strong>!
-        </p>
-        <div className="landing-buttons">
-          <button className="btn btn-driver">Become a Driver</button>
-          <button className="btn btn-passenger">Need a Ride</button>
-        </div>
-      </header>
+      <h1>Welcome to UCSD Campus Ride</h1>
+      <p>
+        <span className="special-text">Organize rides to campus events.</span>{" "}
+        Sign up as a <strong>driver</strong> or find a ride as a{" "}
+        <strong>passenger</strong>!
+      </p>
+      <div className="landing-buttons">
+        <button className="btn btn-driver">Become a Driver</button>
+        <button className="btn btn-passenger">Need a Ride</button>
+      </div>
     </div>
   );
 }

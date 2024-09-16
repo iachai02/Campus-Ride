8757054c29557aa0929560afcd46a47c5a8daeee
Immanuel Chai <immanuelchai7@gmail.com>
2024-09-16 12:18:46 -0700

    finished css for sign up page
    centered the inputs as well as keep the heading and the signup box close together. Styled the button as well

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -37,12 +37,20 @@ const SignUp: React.FC = () => {
         <form>
           <h2>Create a CampusRide account</h2>
           <div className="form-group">
-            <label htmlFor="fullName">Full Name</label>
-            <input type="text" id="fullName" name="fullName" required />
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" name="firstName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" name="lastName" required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
           </div>
           <div className="form-group">
             <button type="submit">Create Account</button>
